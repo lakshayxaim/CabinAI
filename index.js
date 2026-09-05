cd11b81f919a102dfb481369a6e386b597aad060
Lakshay Chawla <lakshayxaim>
2026-09-05 16:59:04 +0530

    chore: initialize project

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello!");
+})
+
+const PORT = 5000;
+
+app.listen(PORT
+    //  () => {
+    // console.log(`Server is running on port no: ${PORT}`);}
+)
